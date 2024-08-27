b2c9891dc9ff538b3a09272f07848e4736b13b22
Tim McGinley <timwasnever@gmail.com>
2024-08-27 12:24:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import foobar.group10.main
+import ifcopenshell
+
+import analyst_gr10.rules
+import analyst_gr11.rules
+
+
+model = ifcopenshell.open('somefile.ifc')
+
+result1 = analyst_gr10.rules.doorCheck.doCheck(model)
+result2 = analyst_gr11.rules.foobarCheck.doCheck(model)
+
+print(result1)
+print(result2)
